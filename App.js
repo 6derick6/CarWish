09748bca86997381860584bb535b66d447b7ecef
Dérick <6derick6@gmail.com>
2023-06-20 13:24:34 -0300

    Ajustes nos tamanhos das imagens

--- App.js
+++ App.js
@@ -14,32 +14,32 @@ function HomeScreen() {
 
           <TouchableOpacity style={styles.box}>
             <View style={{width:'50%',alignItems:'flex-start',paddingLeft:32}}>
-              <Text style={{fontWeight:500, fontSize:11, color:'#AEAEB3',paddingTop:26}}>AUDI</Text>
-              <Text style={{fontWeight:500, fontSize:17, color:'#47474D'}}>RS 3 Coupé</Text>
+              <Text style={{fontWeight:500, fontSize:11, color:'#AEAEB3',paddingTop:26}}>BMW</Text>
+              <Text style={{fontWeight:500, fontSize:17, color:'#47474D'}}>M3 Competition</Text>
               <View style={styles.gasolina}>
                 <Text style={{fontWeight:500, fontSize:15,color:'#47474D'}}>Fuel <Image source={require('./images/gasolina.png')}/></Text>
               </View>
-              <Text style={{fontWeight:500, fontSize:15, color:'#DC1637'}}>R$ 342.000,00</Text>
+              <Text style={{fontWeight:500, fontSize:15, color:'#DC1637'}}>R$ 799.950,00</Text>
             </View>
             <View style={{width:'50%'}}>
-              <View style={styles.imgCarro}>
-                <Image source={require('./images/Audi.png')}/>
+              <View style={styles.carro}>
+                <Image resizeMode='contain' style={styles.imgCarro} source={require('./images/m3_motorista.png')}/>
               </View>
             </View>
           </TouchableOpacity>
 
           <TouchableOpacity style={styles.box}>
             <View style={{width:'50%',alignItems:'flex-start',paddingLeft:32}}>
-              <Text style={{fontWeight:500, fontSize:11, color:'#AEAEB3',paddingTop:26}}>PORSCHE</Text>
-              <Text style={{fontWeight:500, fontSize:17, color:'#47474D'}}>Panamera</Text>
+              <Text style={{fontWeight:500, fontSize:11, color:'#AEAEB3',paddingTop:26}}>BMW</Text>
+              <Text style={{fontWeight:500, fontSize:17, color:'#47474D'}}>530e M SPORT</Text>
               <View style={styles.gasolina}>
-                <Text style={{fontWeight:500, fontSize:15,color:'#47474D'}}>Fuel <Image source={require('./images/gasolina.png')}/></Text>
+                <Text style={{fontWeight:500, fontSize:15,color:'#47474D'}}>Fuel <Image source={require('./images/eletrico.png')}/></Text>
               </View>
-              <Text style={{fontWeight:500, fontSize:15, color:'#DC1637'}}>R$ 420.000,00</Text>
+              <Text style={{fontWeight:500, fontSize:15, color:'#DC1637'}}>R$ 455.950,00</Text>
             </View>
             <View style={{width:'50%'}}>
-              <View style={styles.imgCarro}>
-                <Image source={require('./images/Porche.png')}/>
+              <View style={styles.carro}>
+                <Image resizeMode='contain' style={styles.imgCarro} source={require('./images/bmw_530e.png')}/>
               </View>
             </View>
           </TouchableOpacity>
@@ -82,7 +82,7 @@ const styles = StyleSheet.create({
     marginBottom:20
   },
 
-  imgCarro: {
+  carro: {
     width:'100%',
     height:'100%',
     display:'flex',
@@ -96,4 +96,9 @@ const styles = StyleSheet.create({
   eletrico: {
     paddingTop:16,
   },
+
+  imgCarro: {
+    width:280,
+    marginLeft:-55
+  }
 });
